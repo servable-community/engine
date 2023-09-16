f9f0930202b3fa8dc8d7383b1b1c130abe2aede8
Aboubacar Doucoure <yelounak@gmail.com>
2023-09-16 22:18:31 +0200

    fix(utils): checking if json exists before importing

--- src/utils/importJSONAsync.js
+++ src/utils/importJSONAsync.js
@@ -1,6 +1,10 @@
 import fs from "fs"
+import checkFileExists from "./checkFileExists"
 
 export default async _url => {
+  if (!(await checkFileExists(_url))) {
+    return null
+  }
   const fileUrl = new URL(_url, import.meta.url)
   const parsedPackageJSON = JSON.parse(await fs.promises.readFile(fileUrl, 'utf8'))
   return parsedPackageJSON
