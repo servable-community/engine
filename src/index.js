64bf0d0e08a324ca9002bf3b66b32457e7075350
Aboubacar Doucoure <yelounak@gmail.com>
2023-09-16 20:43:35 +0200

    Fix double from

--- src/index.js
+++ src/index.js
@@ -3,9 +3,8 @@
 // export { default as cleanProtocols } from './utils/cleanProtocols.js'
 
 import launchServable from './launch/index.js'
-import computeSchema from from
+import computeSchema from './lib/schema/compute/index.js'
 import cleanProtocols from './utils/cleanProtocols.js'
-'./lib/schema/compute/index.js'
 
 export default {
   launchServable,
