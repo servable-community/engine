92e3309984c50240ca1355ec89ee8eadc955cff3
Aboubacar Doucoure <yelounak@gmail.com>
2023-09-16 20:41:32 +0200

    fix(module): changed umbrella

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-export { default as launchServable } from './launch/index.js'
-export { default as computeSchema } from './lib/schema/compute/index.js'
-export { default as cleanProtocols } from './utils/cleanProtocols.js'
+// export { default as launchServable } from './launch/index.js'
+// export { default as computeSchema } from './lib/schema/compute/index.js'
+// export { default as cleanProtocols } from './utils/cleanProtocols.js'
+
+import launchServable from './launch/index.js'
+import computeSchema from from
+import cleanProtocols from './utils/cleanProtocols.js'
+'./lib/schema/compute/index.js'
+
+export default {
+  launchServable,
+  computeSchema,
+  cleanProtocols
+}
