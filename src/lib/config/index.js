b7d38118cfcc369aecab6447cb4bc43de884e92e
Aboubacar Doucoure <yelounak@gmail.com>
2023-09-16 20:58:02 +0200

    fix(module): fix for export default as

--- src/lib/config/index.js
+++ src/lib/config/index.js
@@ -1,1 +1,2 @@
-export { default as validate } from './validate/index.js'
+import validate from './validate/index.js'
+export { validate }
--- src/lib/schema/index.js
+++ src/lib/schema/index.js
@@ -1,1 +1,2 @@
-export { default as compute } from './compute/index.js'
+import compute from './compute/index.js'
+export { compute }
