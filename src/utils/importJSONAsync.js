fa09ca04d9f5802820c3881d238321138ec33f13
Aboubacar Doucoure <yelounak@gmail.com>
2023-11-01 11:44:39 +0100

    fix(importjsonasync): added catch

--- src/utils/importJSONAsync.js
+++ src/utils/importJSONAsync.js
@@ -2,10 +2,15 @@ import fs from "fs"
 import checkFileExists from "./checkFileExists.js"
 
 export default async _url => {
-  if (!(await checkFileExists(_url))) {
+  try {
+    if (!(await checkFileExists(_url))) {
+      return null
+    }
+    const fileUrl = new URL(_url, import.meta.url)
+    const parsedPackageJSON = JSON.parse(await fs.promises.readFile(fileUrl, 'utf8'))
+    return parsedPackageJSON
+  } catch (e) {
+    console.log("[Servable]", `importJSONAsync → `, e)
     return null
   }
-  const fileUrl = new URL(_url, import.meta.url)
-  const parsedPackageJSON = JSON.parse(await fs.promises.readFile(fileUrl, 'utf8'))
-  return parsedPackageJSON
 }
