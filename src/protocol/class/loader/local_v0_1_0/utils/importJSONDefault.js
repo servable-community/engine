27ad8c4ee3fac6fbeba2f295d772e683f6d3345c
Aboubacar Doucoure <yelounak@gmail.com>
2023-11-02 12:54:12 +0100

    chore(module): added log to importJS and JSON

--- src/protocol/class/loader/local_v0_1_0/utils/importJSONDefault.js
+++ src/protocol/class/loader/local_v0_1_0/utils/importJSONDefault.js
@@ -1,6 +1,13 @@
 import importJSONAsync from "../../../../../utils/importJSONAsync.js"
 
 export default async ({ path, cache, cacheKey }) => {
-  const data = await importJSONAsync(path)
-  return data
+  try {
+    console.log("[Servable]", `importJSONDefault for path ${path}`)
+    const data = await importJSONAsync(path)
+    console.log("[Servable]", `importJSONDefault for path ${path} successful`)
+    return data
+  } catch (e) {
+    console.error("[Servable]", `importJSONDefault for path ${path}:`, e)
+  }
+  return null
 }
