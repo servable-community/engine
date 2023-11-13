fc550a5f173d965fa6d14cf528ee0bc8311efa74
Aboubacar Doucoure <yelounak@gmail.com>
2023-11-13 13:54:50 +0100

    style(module): removed comments on import json and js

--- src/protocol/class/loader/local_v0_1_0/utils/importJSDefault.js
+++ src/protocol/class/loader/local_v0_1_0/utils/importJSDefault.js
@@ -2,9 +2,9 @@
 
 export default async ({ path, cache, cacheKey }) => {
   try {
-    console.log("[Servable]", `importJSDefault for path ${path}`)
+    // console.log("[Servable]", `importJSDefault for path ${path}`)
     const data = (await import(path)).default
-    console.log("[Servable]", `importJSDefault for path ${path} successful`)
+    // console.log("[Servable]", `importJSDefault for path ${path} successful`)
     return data
   } catch (e) {
     console.error("[Servable]", `importJSDefault for path ${path}:`, e)
--- src/protocol/class/loader/local_v0_1_0/utils/importJSONDefault.js
+++ src/protocol/class/loader/local_v0_1_0/utils/importJSONDefault.js
@@ -2,9 +2,9 @@ import importJSONAsync from "../../../../../utils/importJSONAsync.js"
 
 export default async ({ path, cache, cacheKey }) => {
   try {
-    console.log("[Servable]", `importJSONDefault for path ${path}`)
+    // console.log("[Servable]", `importJSONDefault for path ${path}`)
     const data = await importJSONAsync(path)
-    console.log("[Servable]", `importJSONDefault for path ${path} successful`)
+    // console.log("[Servable]", `importJSONDefault for path ${path} successful`)
     return data
   } catch (e) {
     console.error("[Servable]", `importJSONDefault for path ${path}:`, e)
