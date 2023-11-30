0167a17f57c11af190a0fcc6f180b60b6ac8b8e4
Aboubacar Doucoure <yelounak@gmail.com>
2023-11-30 11:26:58 +0100

    fix(tree): added tree enums

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import launchServable from './launch/index.js'
 import computeSchema from './lib/schema/compute/index.js'
 // import validateProtocol from './lib/config/validate/protocol/index.js'
 import cleanProtocols from './utils/cleanProtocols.js'
+import extractProtocol from './protocol/utils/tree/extract/index.js'
 import protocolManifest from './protocol/utils/tree/access/index.js'
+import * as TreeEnums from './protocol/utils/tree/enums.js'
 import documentProtocol from './protocol/utils/documentation/index.js'
 
 export {
@@ -10,6 +12,8 @@ export {
   computeSchema,
   cleanProtocols,
   protocolManifest,
-  documentProtocol
+  documentProtocol,
+  extractProtocol,
+  TreeEnums
   // validateProtocol
 }
--- src/launch/index.js
+++ src/launch/index.js
@@ -13,7 +13,7 @@ import printEnd from './_messages/end.js'
 import launchSystem from "./system/index.js"
 import { compute } from "../lib/schema/index.js"
 import config from "./config/index.js"
-import documentProtocol from '../protocol/utils/documentation/index.js'
+// import documentProtocol from '../protocol/utils/documentation/index.js'
 
 export default async props => {
   const { servableEngineConfig } = props
@@ -41,17 +41,17 @@ export default async props => {
       console.log("[Servable]", `Launch > failed creating the parse server`)
       return
     }
-    const ccs = []
-    // await Promise.all([_schema.protocols[0]].map(async schema => {
-    await Promise.all(_schema.protocols.map(async (schema, index) => {
-      if (index === 17) {
-        const dic = await documentProtocol({
-          path: schema.loader.path,
-          write: true
-        })
-        console.log(dic)
-      }
-    }))
+
+    // // await Promise.all([_schema.protocols[0]].map(async schema => {
+    // await Promise.all(_schema.protocols.map(async (schema, index) => {
+    //   if (index === 17) {
+    //     const dic = await documentProtocol({
+    //       path: schema.loader.path,
+    //       write: true
+    //     })
+    //     console.log(dic)
+    //   }
+    // }))
 
     console.log("[Servable]", `Launch > started the parse server`)
 
