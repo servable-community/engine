63e4b12826550983e589c4d825160d13571bbf22
Aboubacar Doucoure <yelounak@gmail.com>
2023-12-21 10:02:33 +0100

    fix(boot): added frameworkAdapter

--- src/launch/start/boot/index.js
+++ src/launch/start/boot/index.js
@@ -118,6 +118,7 @@ export default async (props) => {
       migrationPayload: productionMigrations,
       servableEngineConfig,
       configuration: productionConfiguration,
+      frameworkAdapter
     })
 
     if (result.error) {
